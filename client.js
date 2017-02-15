4be51f56a3740a4e50802b25edae5a5efcda2758
Edmund Wippler <edmund@Edmunds-MacBook-Pro.local>
2017-02-15 09:38:25 -0600

    added div and created array of objects

--- client.js
+++ client.js
@@ -1,10 +1,15 @@
-// var colorArray = [{color: 'red', idNumber: 1}, {color: 'green', idNumber: 2}, {color: 'yellow', idNumber: 3}, {color: 'blue', idNumber: 4}];
+var colorArray = [{color: 'Red', idNumber: 1, colorValue: '#ff0000'}, {color: 'Green', idNumber: 2, colorValue: '#00ff00'}, {color: 'Yellow', idNumber: 3, colorValue: '#ffff00'}, {color: 'Blue', idNumber: 4, colorValue: '#0000ff'}, {color: 'Aqua', idNumber: 5, colorValue: '#09c0ff'}, {color: 'Pink', idNumber: 6, colorValue: '#cc00cc'}, {color: 'Orange', idNumber: 7, colorValue: '#ffaf00'}];
 $(document).ready(function() {
   // displays color blocks on page load
+  alert('Please select a difficulty.')
   $('.colorBlock').css("display", "inline-block");
-  alert('Please select a color.')
+  // $('#easy').on('click', function(){
+  //   $('body').append('<div class="colorBlock" ')
+  //
+  // })
+
   //generate random number and check if equal to block id
-  var genNumber = randomNumber(1, 4)
+  var genNumber = randomNumber(1, 5)
   $('div').on('click', function() {
     if ($(this).data().number == genNumber) {
       $('#resultColor').text($(this).data().color);
